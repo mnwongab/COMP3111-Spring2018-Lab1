ff04e7e37a9134f1dfab8305c386279c09b82c17
ManNok <ManNok@10.89.152.133>
2018-02-13 11:45:38 +0800

    Added max and min

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -2,7 +2,25 @@ package labs.comp3111.ust.hk;
 
 public class Lab1 {
 	public static void main(String[] args) {
-		System.out.println("Lab 1: Introduction to Git and Github");
-		System.out.println("Congratulation! You learned the Git Basics");
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += arr[i];
+		}
+		System.out.println(String.format("Sum of the numbers is %d", sum));
+		int min = 0;
+		for (int i = 0; i < 10; i++) {
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		System.out.println(String.format("Min = %d", min));
+		int max = 0;
+		for (int i = 0; i < 10; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println(String.format("Max = %d", max));
 	}
 }
